740ed9e89443a582c24e381dfafdeec818b5359c
Siddhesh Bhosale <bhosalesiddhesh9@gmail.com>
2021-08-23 14:27:47 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -263,7 +263,27 @@ export default function App() {
         <button onClick={e=>clickhandler(data)}>Check</button>
         <div className="display">{output}</div>
         </div>
-      </div> 
+      </div>
+
+      <footer id="footer">
+                <ul className="footer-list">
+                    <li className="list-item"><a target="_blank" href="bhosalesiddhesh9@gmail.com" className="footer-links">
+                        <i className="fa fa-envelope" aria-hidden="true"></i></a>
+                    </li>
+                    <li className="list-item"><a href="https://github.com/Siddheshbhosale">
+                        <i className="fab fa-github" aria-hidden="true"></i>
+                    </a>
+                    </li>
+                    <li className="list-item"><a href="https://www.linkedin.com/in/siddheshbhosale">
+                        <i className="fab fa-linkedin-in"></i>
+                    </a>
+                    </li>
+                    <li className="list-item"><a href="https://github.com/Siddheshbhosale" >
+                        <i className="fas fa-briefcase"></i>
+                    </a>
+                    </li>
+                </ul>
+            </footer> 
     </div>
   );
 }
